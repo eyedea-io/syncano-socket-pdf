179ae964a5fda2f91512151fecd30e07a42bebc1
Kasper Mikiewicz <ideredpl@gmail.com>
2017-08-22 15:31:58 +0200

    feat: implement rendering from argument
    Argument `template` must be html string.  Request must be send ad `x-www-form-urlencoded`

--- index.js
+++ index.js
@@ -1,28 +1,79 @@
 const ElectronPDF = require('electron-pdf')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const fs = require('fs')
 const exporter = new ElectronPDF()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 exporter.on('charged', () => {
   app.listen(3000, 'localhost', () => {})
 
-  app.get('/', (req, res) => {
-    const jobOptions = {inMemory: true, closeWindow: false}
+  app.post('/', (req, res) => {
+    const {template} = req.body
 
-    exporter
-      .createJob('./render.html', './template.pdf', [], jobOptions)
-      .then(job => {
-        job.on('job-complete', r => {
-          res.setHeader('Content-Type', 'application/pdf')
-          res.setHeader(
-            'Content-Disposition',
-            'attachment; filename=' + 'template.pdf'
-          )
-          res.end(r.results[0], 'binary')
-        })
-        job.render()
+    createTemplate(template)
+      .then(queueJob)
+      .then(resolveJob)
+      .then(({pdf, filename}) => {
+        res.setHeader('Content-Type', 'application/pdf')
+        res.setHeader(
+          'Content-Disposition',
+          'attachment; filename=' + `${filename}.pdf`
+        )
+        res.end(pdf, 'binary')
       })
   })
 })
 
 exporter.start()
+
+function createTemplate(template) {
+  return new Promise((resolve, reject) => {
+    const filename = `${hashString(template).toString(36)}`
+
+    fs.writeFile(`./storage/${filename}.html`, template, () => {
+      resolve(filename)
+    })
+  })
+}
+
+function queueJob(filename) {
+  const jobOptions = {
+    inMemory: true,
+    closeWindow: false
+  }
+
+  return exporter
+    .createJob(`./storage/${filename}.html`, `${filename}.pdf`, [], jobOptions)
+    .then(job => ({job, filename}))
+}
+
+function resolveJob({job, filename}) {
+  return new Promise(resolve => {
+    job.on('job-complete', r => {
+      resolve({pdf: r.results[0], filename})
+      fs.unlink(`./storage/${filename}.html`)
+    })
+
+    job.render()
+  })
+}
+
+function hashString(str, seed) {
+  let hash = 0
+  let i
+  let chr
+
+  if (str.length === 0) return hash
+
+  for (i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
+    hash |= 0
+  }
+
+  return hash
+}
