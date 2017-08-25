c0db493d77064724101985f8a78c4cf4de801d34
Kasper Mikiewicz <ideredpl@gmail.com>
2017-08-25 16:10:51 +0200

    chore: change localhost to 0.0.0.0

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({limit: '5mb'}))
 app.use(bodyParser.urlencoded({extended: true, limit: '5mb'}))
 
 exporter.on('charged', () => {
-  app.listen(3000, 'localhost', () => {})
+  app.listen(3000, '0.0.0.0', () => {})
 
   app.post('/', (req, res) => {
     const {template} = req.body
